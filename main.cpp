2f7423f6ef85c810a579a0f3c9c02eedba6b697f
Ouiam-lhj <ouiam.el-hajji@insa-lyon.fr>
2025-01-14 20:28:49 +0100

    Added a main

---
+++ main.cpp
@@ -0,0 +1,11 @@
+// hello
+#include <iostream>
+#include <vector>
+#include "Character.h"
+#include "Mario.h"
+#include "Yoshi.h"
+using namespace std;
+
+int main() {
+   cout << "Hello World!" << endl;
+}
