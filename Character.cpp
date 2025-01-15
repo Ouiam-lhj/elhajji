7c75430f0dccd227d68868fe7d9683644c9ada1d
Ouiam-lhj <ouiam.el-hajji@insa-lyon.fr>
2025-01-15 22:02:00 +0100

    added character.cpp (construc/destruc)

---
+++ Character.cpp
@@ -0,0 +1,14 @@
+#include "Character.h"
+
+#include <iostream>
+#include <string>
+
+//constructeur par défaut
+Character::Character(){
+	speed_=0;
+	max_speed_ = 10;
+}
+
+//Destructeur
+Character::~Character() = default;
+
